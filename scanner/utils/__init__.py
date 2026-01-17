183f26d55114b35ea4a452c2511c0df9d72f3ec3
schluchtenscheisser <martinteiber@gmx.at>
2026-01-17 09:04:06 +0100

    Update __init__.py

--- scanner/utils/__init__.py
+++ scanner/utils/__init__.py
@@ -1,1 +1,9 @@
+"""
+Utility helpers for the scanner.
+
+Modules:
+- time_utils: time and date handling
+- logging_utils: logging configuration helpers
+- io_utils: file I/O helpers (JSON, Markdown, paths)
+"""
 
